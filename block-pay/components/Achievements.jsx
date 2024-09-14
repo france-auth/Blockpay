67bcf4e0b27aeee5801d6fd80ebed4319e416f80
Francis Authority <francis.i.authority@gmail.com>
2024-09-15 01:46:53 +0200

    ....

--- block-pay/components/Achievements.jsx
+++ block-pay/components/Achievements.jsx
@@ -7,7 +7,7 @@ const Achievements = () => {
   return (
     <section className="w-full px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 mt-8 mb-5 md:mb-2">
       <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 mt-8 mb-5 md:mb-2">
+        <div className="flex flex-col md:flex-row md:justify-between sm:items-center px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 mt-8 mb-5 md:mb-2">
           <div className="flex flex-col justify-center mb-6 sm:mb-0">
             <h1 className="text-head text-color text-2xl sm:text-3xl font-semibold">
               Our Success<br />so far
